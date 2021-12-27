5ecbf6b117eb86e145821e28e6533fac8c7f776a
Jeff <zhsu@amwares.com>
2021-12-27 18:10:01 +0800

    第一步就很麻烦，看来不能在运行时取得尺寸，只能做一套不同分辨率的尺寸资源。

---
+++ app/src/main/java/com/amware/c3s15/ChessBoard.kt
@@ -0,0 +1,20 @@
+package com.amware.c3s15
+
+import android.content.Context
+import android.graphics.Canvas
+import android.view.View
+
+class ChessBoard(context: Context) : View(context) {
+
+	override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+		// super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+		// val x = (parent as MainActivity).tv01.width / 2
+		// setMeasuredDimension(x, x)
+		setMeasuredDimension(200, 200)
+	}
+
+	override fun onDraw(canvas: Canvas?) {
+		super.onDraw(canvas)
+	}
+
+}
--- app/src/main/java/com/amware/c3s15/MainActivity.kt
+++ app/src/main/java/com/amware/c3s15/MainActivity.kt
@@ -2,10 +2,15 @@ package com.amware.c3s15
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : Activity() {
+
+//	val tv01 = findViewById<TextView>(R.id.tv01)
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 	}
+
 }
