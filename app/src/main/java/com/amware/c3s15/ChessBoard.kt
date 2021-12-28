0cda2a1e75e35563035972de1b100c1d3ee7007b
Jeff <zhsu@amwares.com>
2021-12-28 15:49:34 +0800

    这种适配方法真笨！还漏写了单位，又要逐个去改，烦！要想想别的办法。

--- app/src/main/java/com/amware/c3s15/ChessBoard.kt
+++ app/src/main/java/com/amware/c3s15/ChessBoard.kt
@@ -6,12 +6,12 @@ import android.view.View
 
 class ChessBoard(context: Context) : View(context) {
 
-	override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-		// super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-		// val x = (parent as MainActivity).tv01.width / 2
-		// setMeasuredDimension(x, x)
-		setMeasuredDimension(200, 200)
-	}
+//	override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+//		// super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+//		// val x = (parent as MainActivity).tv01.width / 2
+//		// setMeasuredDimension(x, x)
+//		setMeasuredDimension(200, 200)
+//	}
 
 	override fun onDraw(canvas: Canvas?) {
 		super.onDraw(canvas)
