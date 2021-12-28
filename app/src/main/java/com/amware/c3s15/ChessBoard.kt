6ea6b0a0ad030a6ec8352cff5979cde2aa69dfa6
Jeff <zhsu@amwares.com>
2021-12-28 17:01:42 +0800

    仍然问题多多，再想办法。

--- app/src/main/java/com/amware/c3s15/ChessBoard.kt
+++ app/src/main/java/com/amware/c3s15/ChessBoard.kt
@@ -2,16 +2,24 @@ package com.amware.c3s15
 
 import android.content.Context
 import android.graphics.Canvas
-import android.view.View
+import android.widget.LinearLayout
 
-class ChessBoard(context: Context) : View(context) {
+class ChessBoard(context: Context) : LinearLayout(context) {
 
-//	override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-//		// super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-//		// val x = (parent as MainActivity).tv01.width / 2
-//		// setMeasuredDimension(x, x)
+	override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+		// super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+		// val x = (parent as MainActivity).tv01.width / 2
+		// setMeasuredDimension(x, x)
 //		setMeasuredDimension(200, 200)
-//	}
+
+		setMeasuredDimension(
+			getDefaultSize(0, widthMeasureSpec),
+			getDefaultSize(0, heightMeasureSpec)
+		)
+		val childWidthSize = measuredWidth
+		val actualSize = MeasureSpec.makeMeasureSpec(childWidthSize, MeasureSpec.EXACTLY)
+		super.onMeasure(actualSize, actualSize)
+	}
 
 	override fun onDraw(canvas: Canvas?) {
 		super.onDraw(canvas)
